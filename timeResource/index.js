6e7b59dc153d2c53da37300064f4c64d3effda5f
Abhishek Bhatt <abhishek.bhatt@hotmail.com>
2018-04-05 11:48:58 +0530

    test change

--- timeResource/index.js
+++ timeResource/index.js
@@ -4,9 +4,7 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-
        body: 'The time in Los Angeles is please take a note of this...new: ' + currentTime.toString(),
-
 
     });
 };
